7e75add861fd5b59cba658aa54664872df2cdf39
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-09-04 12:06:05 +0400

    top category changes

--- src/components/TopBlock/TopBlock.tsx
+++ src/components/TopBlock/TopBlock.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import styles from "./TopBlock.module.scss";
 import flower from "@/assets/images/flower.png";
+import flowerSmall from "@/assets/images/flowerSmall.png";
+import lineTo from "@/assets/icons/lineTo.svg";
+import info from "@/assets/icons/info.svg";
 import txt1 from "@/assets/icons/Ботаническая1.svg";
 import txt2 from "@/assets/icons/Ботаническая2.svg";
 import { useIsMobile } from "@/hooks/useIsMobile";
@@ -13,9 +16,38 @@ const TopBlock: React.FC = () => {
   return (
     <section className={styles.topBlockContainer}>
       <article className={styles.left}>
-        <div className={styles.imageWrapper}>
-          <img src={txt2} alt="text" />
-          <img src={txt1} alt="text2" />
+        <div className={styles.wrapper}>
+          {isMobile ? (
+            <div className={styles.marqueeContent}>
+              <p className={styles.textIcon}>Ботаническая</p>
+              <p className={styles.textIcon}>Косметика</p>
+            </div>
+          ) : (
+            <div className={styles.imageWrapper}>
+              <img src={txt2} alt="text" />
+              <img src={txt1} alt="text2" />
+            </div>
+          )}
+          <div className={styles.content}>
+            <img src={flowerSmall} alt="flowerSmall" />
+            {isMobile ? (
+              <div className={styles.textWrapper}>
+                <p className={styles.textWrapperTitle}>Предложения</p>
+                <p className={styles.textWrappertxt}>Подобрать уход</p>
+                <p className={styles.textWrappertxt}>Акции</p>
+              </div>
+            ) : (
+              <div className={styles.textWrapper}>
+                <p className={styles.textWrapperTitle}>Подобрать уход</p>
+                <p className={styles.textWrappertxt}>Программа благодарности</p>
+                <p className={styles.textWrappertxt}>
+                  Шаг за шагом к чистой коже
+                </p>
+              </div>
+            )}
+            <img src={lineTo} alt="text" className={styles.lineTo} />
+            <img src={info} alt="text2" className={styles.info} />
+          </div>
         </div>
       </article>
       <article className={styles.right}>
