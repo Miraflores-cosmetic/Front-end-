66764e60f4068d2ea9dfa70a3612d4b6f3992af5
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-09-06 17:07:05 +0400

    Refactoring cart text block

--- src/components/bestsellers/Bestsellers.tsx
+++ src/components/bestsellers/Bestsellers.tsx
@@ -80,9 +80,30 @@ export default function Bestsellers() {
     arrows: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 3.1,
+    slidesToShow: 3.5,
     slidesToScroll: 1,
     responsive: [
+      {
+        breakpoint: 1750,
+        settings: {
+          slidesToShow: 3.3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 3.2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 3.1,
+          slidesToScroll: 1,
+        },
+      },
       {
         breakpoint: 1000,
         settings: {
@@ -95,19 +116,72 @@ export default function Bestsellers() {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1.1,
+          slidesToShow: 1.3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1.2,
           slidesToScroll: 1,
           arrows: false,
           centerMode: false,
         },
       },
       {
-        breakpoint: 450,
+        breakpoint: 610,
         settings: {
           slidesToShow: 1.1,
+
           slidesToScroll: 1,
-          arrows: false,
-          centerMode: false,
+          dots: false,
+        },
+      },
+
+      {
+        breakpoint: 567,
+        settings: {
+          slidesToShow: 1.3,
+
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1.2,
+
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 470,
+        settings: {
+          slidesToShow: 1.1,
+
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1.2,
+
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 430,
+        settings: {
+          slidesToShow: 1.1,
+
+          slidesToScroll: 1,
           dots: false,
         },
       },
