cd88c5c7bf780ccae017ee17ac6b75939064af04
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-09-08 11:12:33 +0400

    Update steps component

--- src/components/steps-block/StepsBlock.tsx
+++ src/components/steps-block/StepsBlock.tsx
@@ -1,70 +1,57 @@
 import React from "react";
 import styles from "./StepsBlock.module.scss";
-import girlwithsmile from "@/assets/images/girlsmile.webp";
+import shablon1 from "@/assets/images/shablon1.webp";
+import shablon2 from "@/assets/images/shablon2.webp";
+import shablon3 from "@/assets/images/shablon3.webp";
+import shablon4 from "@/assets/images/shablon4.webp";
+import etap1 from "@/assets/images/etap1.webp";
+import etap2 from "@/assets/images/etap2.webp";
+import etap3 from "@/assets/images/etap3.webp";
+import etap4 from "@/assets/images/etap4.webp";
 import Step from "./step/Step";
 
 const steps = [
   {
     id: 1,
-    title: "Энзимный мусс для умывания",
-    description: "Энзимы риса + фруктовые энзимы и фруктовые кислоты",
-    price: 3590,
-    oldPrice: 4600,
-    discount: 22,
-    image: girlwithsmile,
+    title: "Очищение",
+    description:
+      "Самое важное выбирать хороший составы для очищения. Наши продукты мягко удаляют загрязнения, не вызывая чувства стянутости.",
+    image: shablon1,
+    hoverImage: etap1,
   },
   {
     id: 2,
     title: "Энзимный мусс для умывания",
     description: "Энзимы риса + фруктовые энзимы и фруктовые кислоты",
-    price: 3590,
-    oldPrice: 4600,
-    discount: 22,
-    image: girlwithsmile,
+    image: shablon2,
+    hoverImage: etap2,
   },
   {
     id: 3,
     title: "Цветочный мист",
     description: "Мист для мягкой и сияющей кожи с экстрактом розы",
-    price: 3590,
-    oldPrice: 4600,
-    discount: 23,
-    image: girlwithsmile,
+    image: shablon3,
+    hoverImage: etap3,
   },
   {
     id: 4,
     title: "Цветочный мист",
     description: "Мист для мягкой и сияющей кожи с экстрактом розы",
-    price: 3590,
-    oldPrice: 4600,
-    discount: 23,
-    image: girlwithsmile,
+    image: shablon4,
+    hoverImage: etap4,
   },
 ];
 
 export default function StepsBlock() {
   return (
     <section className={styles.stepsContainer}>
-      <div className={styles.titleWrapper}>
-        <p className={styles.title}>каждый шаг усиливает</p>
-        <p className={styles.title}>предыдущий</p>
-      </div>
-
-      <div className={styles.descWrapper}>
-        <p className={styles.desc}>
-          Знали ли вы, что даже самое эффективное средство не сработает, если
-          кожа неправильно очищена.
-        </p>
-        <p className={styles.desc}>
-          Агрессивное умывание всего за минуту может нарушить защитный барьер и
-          сделать кожу уязвимой
-        </p>
-      </div>
       <div className={styles.stepsWrapper}>
-        {steps.map((product) => (
+        {steps.map((product, id) => (
           <Step
+            etap={id + 1}
             key={product.id}
             image={product.image}
+            hoverImage={product.hoverImage}
             title={product.title}
             description={product.description}
           />
--- src/components/steps-block/step/Step.tsx
+++ src/components/steps-block/step/Step.tsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Step.module.scss";
 
 interface StepProps {
   image: string;
+  hoverImage: string;
   title: string;
   description: string;
+  etap: number;
 }
 
-const Step: React.FC<StepProps> = ({ image, title, description }) => {
+const Step: React.FC<StepProps> = ({
+  image,
+  hoverImage,
+  title,
+  description,
+  etap,
+}) => {
+  const [isHovered, setIsHover] = useState(false);
+
   return (
-    <div className={styles.step}>
-      <img src={image} alt={title} className={styles.stepImage} />
+    <div
+      className={styles.step}
+      // onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      {isHovered ? (
+        <div className={styles.stepWrapper}>
+          <p>Этап{etap}</p>
+          <img src={hoverImage} alt={title} className={styles.stepImage} />
+        </div>
+      ) : (
+        <div className={styles.stepWrapper}>
+          <p>Этап{etap}</p>
+          <img src={image} alt={title} className={styles.stepImage} />
+        </div>
+      )}
       <div className={styles.textContent}>
         <h3>{title}</h3>
         <p>{description}</p>
