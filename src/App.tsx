e818300188506bdb850cd805815fcee48c65bcfd
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-09-22 13:03:09 +0400

    Added sign-in page

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import Catalog from "./pages/Catalog/Catalog";
 
 import DrawerWrapper from "./components/drawers/DrawerWrapper";
 import SignIn from "./pages/SignIn/SignIn";
-import SignUp from "./pages/SignUp/SignUp";
-import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
-import EmailConfirmation from "./pages/  EmailConfirmation/EmailConfirmation";
-import ResetPassword from "./pages/ResetPassword/ResetPassword";
+// import SignUp from "./pages/SignUp/SignUp";
+// import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+// import EmailConfirmation from "./pages/  EmailConfirmation/EmailConfirmation";
+// import ResetPassword from "./pages/ResetPassword/ResetPassword";
 
 const App: React.FC = () => {
   return (
@@ -16,10 +16,10 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/email-confirmation" element={<EmailConfirmation />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        {/* <Route path="/sign-up" element={<SignUp />} /> */}
+        {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+        {/* <Route path="/email-confirmation" element={<EmailConfirmation />} /> */}
+        {/* <Route path="/reset-password" element={<ResetPassword />} /> */}
 
         <Route path="/catalog/" element={<Catalog />} />
         <Route path="/bestseller/" element={<div>bastreller page</div>} />
