e47d5b07d2af1e39decf4fb26d34c2f2dc1759ec
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-09-29 15:52:52 +0400

    refactoring

--- src/pages/Face/face-card/FaceCard.tsx
+++ src/pages/Face/face-card/FaceCard.tsx
@@ -42,7 +42,7 @@ export const FaceCard: React.FC<{ product: Product }> = ({ product }) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className={styles.wrapper}>
+      <div className={styles.wrapperImage}>
         <figure className={styles.imageContens}>
           <img src={isHovered ? product.hoverImage : product.image} alt="" />
         </figure>
@@ -77,17 +77,17 @@ export const FaceCard: React.FC<{ product: Product }> = ({ product }) => {
             <button className={styles.size100}>100 мл</button>
           </div>
         )}
-        <div className={styles.info}>
-          <div className={styles.txtWrapper}>
-            <h3 className={styles.name}>{product.title}</h3>
-            <p className={styles.desc}>{product.description}</p>
-          </div>
-          <div className={styles.priceWrapper}>
-            {product.oldPrice && (
-              <span className={styles.oldPrice}>{product.oldPrice}₽</span>
-            )}
-            <span className={styles.price}>{product.price}₽</span>
-          </div>
+      </div>
+      <div className={styles.info}>
+        <div className={styles.txtWrapper}>
+          <h3 className={styles.name}>{product.title}</h3>
+          <p className={styles.desc}>{product.description}</p>
+        </div>
+        <div className={styles.priceWrapper}>
+          {product.oldPrice && (
+            <span className={styles.oldPrice}>{product.oldPrice}₽</span>
+          )}
+          <span className={styles.price}>{product.price}₽</span>
         </div>
       </div>
     </div>
