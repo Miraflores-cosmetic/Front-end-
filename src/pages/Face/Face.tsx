f27ba8364a0fa57a20d25ddf7e0e3dc6b6341ead
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-09-30 10:27:31 +0400

    Updates

--- src/pages/Face/Face.tsx
+++ src/pages/Face/Face.tsx
@@ -9,10 +9,16 @@ import krem from "@/assets/images/krem.webp";
 import kosmetika from "@/assets/images/kosmetika.png";
 
 import girlwithsmile from "@/assets/images/girlsmile.webp";
+import etap1 from "@/assets/images/etap1.webp";
+import etap2 from "@/assets/images/etap2.webp";
+import etap3 from "@/assets/images/etap3.webp";
+import etap4 from "@/assets/images/etap4.webp";
 import { FaceCard } from "./face-card/FaceCard";
+import Slider from "react-slick";
+import { useScreenMatch } from "@/hooks/useScreenMatch";
 
 const FacePage: React.FC = () => {
-  // const isMobile = useScreenMatch(756);
+  const isMobile = useScreenMatch(756);
   const products = [
     {
       id: 1,
@@ -78,6 +84,51 @@ const FacePage: React.FC = () => {
       type: "sun",
     },
   ];
+
+  const images = [
+    {
+      id: 1,
+      title: "Волосы",
+      image: etap1,
+    },
+    {
+      id: 2,
+      title: "Лицо",
+      image: etap2,
+    },
+    {
+      id: 3,
+      title: "Лицо",
+      image: etap3,
+      discount: "10%",
+    },
+    {
+      id: 4,
+      title: "Лицо",
+      image: etap4,
+      discount: "10%",
+    },
+    {
+      id: 5,
+      title: "Лицо",
+      image: krem,
+      discount: "10%",
+    },
+    {
+      id: 6,
+      title: "Лицо",
+      image: krem,
+    },
+  ];
+
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+  };
   return (
     <article className={styles.faceContainer}>
       <Header />
@@ -91,6 +142,26 @@ const FacePage: React.FC = () => {
           <FaceCard key={product.id} product={product} />
         ))}
       </section>
+      {!isMobile && (
+        <section>
+          <p className={styles.title}>КАТЕГОРИИ</p>
+          <article>
+            <Slider {...settings} className={styles.imageSlider}>
+              {images.map((product) => (
+                <article className={styles.imagesWrapper}>
+                  <img src={product.image} className={styles.slideImage} />
+                  <div className={styles.discountWrapper}>
+                    <p className={styles.name}>{product.title}</p>
+                    {product.discount && (
+                      <p className={styles.discount}>{product.discount}</p>
+                    )}
+                  </div>
+                </article>
+              ))}
+            </Slider>
+          </article>
+        </section>
+      )}
       <Footer footerImage={footerImage} />
     </article>
   );
