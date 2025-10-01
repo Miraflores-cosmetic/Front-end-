bf7d6d32694c4a1a74bda14d9eaf3e19d10593d5
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-10-01 11:26:34 +0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import ForgotPassword from "@/pages/ForgotPassword/ForgotPassword";
 import EmailConfirmation from "@/pages/EmailConfirmation/EmailConfirmation";
 import ResetPassword from "@/pages/ResetPassword/ResetPassword";
 import BestSeller from "@/pages/BestSeller/BestSeller";
-import FacePage from "./pages/Face/Face";
+// import FacePage from "./pages/Face/Face";
 
 const App: React.FC = () => {
   return (
@@ -25,7 +25,7 @@ const App: React.FC = () => {
 
         <Route path="/catalog/" element={<Catalog />} />
         <Route path="/bestseller" element={<BestSeller />} />
-        <Route path="/face" element={<FacePage />} />
+        {/* <Route path="/face" element={<FacePage />} /> */}
       </Routes>
 
       <DrawerWrapper />
