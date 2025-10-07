b864b36afa8665e7a8c5ceb9e3a91fa292dc2218
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-10-07 16:52:57 +0400

    Update

--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -5,8 +5,23 @@ import goBack from "@/assets/icons/go-back.svg";
 import Karta from "@/assets/icons/Karta.svg";
 import SberPay from "@/assets/icons/SberPay.svg";
 import SBP from "@/assets/icons/SBP.svg";
+import Miraflores_logo from "@/assets/icons/Miraflores_logo.svg";
+import krem from "@/assets/images/krem.webp";
 import CustomCheckbox from "@/components/custom-checkBox/CustomCheckbox";
 import CustomButton from "@/components/custom-button/CustomButton";
+
+interface CartItem {
+  id: number;
+  image: string;
+  alt: string;
+  name: string;
+  size: string;
+  discount: string;
+  count: string;
+  priceOld?: string;
+  priceNew?: string;
+  isGift?: boolean;
+}
 
 const Order: React.FC = () => {
   const [name, setName] = useState("Фёдор Ники́форович Плевако́");
@@ -18,6 +33,31 @@ const Order: React.FC = () => {
     { src: SBP, alt: "goSBPBack" },
     { src: Karta, alt: "Karta" },
     { src: SberPay, alt: "SberPay" },
+  ];
+
+  const cartData: CartItem[] = [
+    {
+      id: 1,
+      image: krem,
+      alt: "krem",
+      name: "Цветочный мист с экстрактами розы",
+      size: "50 мл",
+      discount: "-23%",
+      count: "1 шт.",
+      priceOld: "4800₽",
+      priceNew: "3590₽",
+      isGift: false,
+    },
+    {
+      id: 2,
+      image: krem,
+      alt: "tonic",
+      name: "Тоник для лица с розой",
+      size: "100 мл",
+      discount: "",
+      count: "1 шт.",
+      isGift: true,
+    },
   ];
   return (
     <main className={styles.orderContainer}>
@@ -78,7 +118,50 @@ const Order: React.FC = () => {
           </p>
         </section>
       </section>
-      <section className={styles.right}></section>
+      <section className={styles.right}>
+        <article className={styles.listWrapper}>
+          <img
+            src={Miraflores_logo}
+            alt={"Miraflores_logo"}
+            className={styles.Miraflores_logo}
+          />
+          {cartData.map((item) => (
+            <div className={styles.orderCart} key={item.id}>
+              <figure className={styles.cartImageWrapper}>
+                <img
+                  src={item.image}
+                  alt={item.alt}
+                  className={styles.kremImage}
+                />
+              </figure>
+              <div className={styles.infoWrapper}>
+                <div className={styles.top}>
+                  <div className={styles.texts}>
+                    <p className={styles.name}>{item.name}</p>
+                    <p className={styles.size}>{item.size}</p>
+                  </div>
+                  {item.discount && (
+                    <div className={styles.discount}>{item.discount}</div>
+                  )}
+                </div>
+
+                <div className={styles.bottom}>
+                  <p className={styles.count}>{item.count}</p>
+
+                  {item.isGift ? (
+                    <div className={styles.surprise}>Подарок</div>
+                  ) : (
+                    <div className={styles.price}>
+                      <p className={styles.priceOld}>{item.priceOld}</p>
+                      <p className={styles.priceNew}>{item.priceNew}</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+        </article>
+      </section>
     </main>
   );
 };
