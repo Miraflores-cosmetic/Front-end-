0d207013c4cae8e6e67a34669bee2b504fc39223
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-10-07 15:21:05 +0400

    Added layout for order page

---
+++ src/components/custom-checkBox/CustomCheckbox.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "./CustomChecBox.module.scss";
+
+interface CustomCheckboxProps {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+  borderRadius?: number | string; // ← добавили
+}
+
+const CustomCheckbox: React.FC<CustomCheckboxProps> = ({
+  checked,
+  onChange,
+  borderRadius = 4, // ← значение по умолчанию
+}) => {
+  return (
+    <label className={styles.wrapper}>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+        className={styles.input}
+      />
+      <span
+        className={styles.box}
+        style={{
+          borderRadius:
+            typeof borderRadius === "number"
+              ? `${borderRadius}px`
+              : borderRadius,
+        }}
+      >
+        <svg
+          className={styles.check}
+          viewBox="0 0 24 24"
+          width="12"
+          height="12"
+          aria-hidden="true"
+        >
+          <polyline
+            points="20 6 9 17 4 12"
+            fill="none"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </span>
+    </label>
+  );
+};
+
+export default CustomCheckbox;
--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -2,12 +2,23 @@ import React, { useState } from "react";
 import styles from "./Order.module.scss";
 import Input from "@/components/text-field/input/Input";
 import goBack from "@/assets/icons/go-back.svg";
+import Karta from "@/assets/icons/Karta.svg";
+import SberPay from "@/assets/icons/SberPay.svg";
+import SBP from "@/assets/icons/SBP.svg";
+import CustomCheckbox from "@/components/custom-checkBox/CustomCheckbox";
+import CustomButton from "@/components/custom-button/CustomButton";
 
 const Order: React.FC = () => {
   const [name, setName] = useState("Фёдор Ники́форович Плевако́");
   const [emal, setEmail] = useState("f.plevako@gmail.com");
   const [phone, setPhone] = useState("+7(913) 910 30-70");
+  const [checked, setChecked] = useState(true);
 
+  const paymentImages = [
+    { src: SBP, alt: "goSBPBack" },
+    { src: Karta, alt: "Karta" },
+    { src: SberPay, alt: "SberPay" },
+  ];
   return (
     <main className={styles.orderContainer}>
       <section className={styles.left}>
@@ -33,6 +44,38 @@ const Order: React.FC = () => {
             type="number"
             placeholder="Phone"
           />
+        </section>
+        <section className={styles.checkWrapper}>
+          <CustomCheckbox
+            checked={checked}
+            onChange={setChecked}
+            // borderRadius={8}
+          />
+          <label>Пишите мне о распродажах, скидках и новых поступлениях</label>
+        </section>
+        <section className={styles.paymentWrapper}>
+          <p className={styles.paymentTitle}>Оплата</p>{" "}
+          <article className={styles.payments}>
+            {paymentImages.map((item, index) => (
+              <img
+                key={index}
+                src={item.src}
+                alt={item.alt}
+                className={styles.paymentImage}
+              />
+            ))}
+          </article>
+        </section>
+        <section className={styles.orderButtonWrapper}>
+          <figure className={styles.orderButton}>
+            <CustomButton label="Оплатить" onClick={() => alert("ok")} />
+          </figure>
+          <p className={styles.agreement}>
+            Нажимая на кнопку «Оформить заказ», я соглашаюсь с условиями{" "}
+            <span>Публичной оферты</span> и выражаю своё согласие на обработку
+            моих персональных данных в соответствии с{" "}
+            <span>Политикой конфиденциальности</span>
+          </p>
         </section>
       </section>
       <section className={styles.right}></section>
