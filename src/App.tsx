d9e7a9b1eb33cbce52512d9b62bae0ddc59f50b1
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-10-13 16:44:38 +0400

    Added pages

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import BestSeller from "@/pages/BestSeller/BestSeller";
 import FacePage from "./pages/Face/Face";
 import Articles from "./pages/Articles/Articles";
 import ArticleDetail from "./pages/ArticleDetail/ArticleDetail";
+import Order from "./pages/Order/Order";
+import ProfilePage from "./pages/Profile/Profile";
 
 const App: React.FC = () => {
   return (
@@ -28,6 +30,8 @@ const App: React.FC = () => {
         <Route path="/face" element={<FacePage />} />
         <Route path="about/articles" element={<Articles />} />
         <Route path="about/articles/:id" element={<ArticleDetail />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
 
       <DrawerWrapper />
