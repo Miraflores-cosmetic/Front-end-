a552ff1f0a5e5255a61c7cffb9de84aad044402b
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-10-18 13:07:03 +0400

    Update

--- src/pages/Profile/contents/orders-content/OrdersContent.tsx
+++ src/pages/Profile/contents/orders-content/OrdersContent.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import styles from "./OrdersContent.module.scss";
+import Bestsellers from "@/components/bestsellers/Bestsellers";
 
 const OrdersContent: React.FC = () => {
-  return <main className={styles.ordersContent}>ordersContent</main>;
+  return (
+    <article className={styles.ordersContent}>
+      <section className={styles.ordersTitleWrapper}>
+        <p className={styles.ordersTitle}>Ваши заказы</p>
+      </section>
+
+      <section className={styles.ordersWrapper}>
+        <p>ddddddddd</p>
+      </section>
+    </article>
+  );
 };
 
 export default OrdersContent;
